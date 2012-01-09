8a2a07098f6e86dc388a85c89179601dd5c93a79
Eric Chan <kakchan@gmail.com>
2012-01-10 01:32:03 +1100

    add top nav for Users/Posts add Logout button to top nav

--- app/controllers/session_controller.js
+++ app/controllers/session_controller.js
@@ -1,5 +1,5 @@
 load('application');
-before(use('alreadyLoggedIn'));
+before(use('alreadyLoggedIn'), { only: [ 'index', 'create' ] });
 
 action( 'index', function() {
 	this.title = 'Logon';
@@ -14,4 +14,9 @@ action('create', function() {
 		flash('logon unsuccessful');
 		redirect(path_to.session);
 	}
+} );
+
+action('destroy', function() {
+	delete req.session.user_id;
+	redirect(path_to.session);
 } );
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,9 @@
 exports.routes = function (map) {
 	map.resources('users');
 	map.resources('posts');
-	map.resources('sessions');
-	map.get('/', 'sessions#index');
+	map.resources('session');
+	map.get('/logout', 'session#destroy');
+	map.get('/', 'session#index');
 
 	// Generic routes. Add all your routes below this line
 	// feel free to remove generic routes
