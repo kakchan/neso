79932136daa124f8c598ff84914ab0f2f90cd658
Eric Chan <kakchan@gmail.com>
2012-01-15 23:57:33 +1100

    update mimas to use 3001 port by default

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = module.exports = require('railway').createServer(),
 		MemoryStore = require('connect/lib/middleware/session/memory');
 
 if (!module.parent) {
-    var port = process.env.PORT || 3000;
+    var port = process.env.PORT || 3001;
 		app.use(express.cookieParser());
 		app.use(express.session( {
 			store: new MemoryStore( {
